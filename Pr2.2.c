1f7d110db0fc96322c3902f33536fdc4da214d80
RomanSchur <161126957+RomanSchur@users.noreply.github.com>
2025-03-07 14:47:19 +0200

    Add files via upload

---
+++ Pr2.2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int global_arr [1000] ={1};
+void test_function() {    
+    int arr_initialized[1000] = {1, 2, 3, 4};
+    int arr_uninitialized[1000];
+}
+int main() {
+    printf("Hello World!!!!\n");
+    test_function();
+    return 0;
+}
